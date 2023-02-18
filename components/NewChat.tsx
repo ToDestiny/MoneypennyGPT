a83e157b1a5fdfb7b7d4a68b6931e1fc8dbb87ad
Anthony <anthonycolas.france@gmail.com>
2023-02-18 22:26:34 +0100

    New chat button

--- components/NewChat.tsx
+++ components/NewChat.tsx
@@ -1,9 +1,9 @@
-import { PlusIcon } from '@heroicons/react/24/solid';
-import React from 'react';
+import { PlusIcon } from "@heroicons/react/24/solid";
+import React from "react";
 
 function NewChat() {
   return (
-    <div>
+    <div className="border-grey-700 border chatRow">
       <PlusIcon className="h-4 w-4" />
       <p>New Chat</p>
     </div>

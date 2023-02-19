82b37677e94b006ac1e306e67b80e6df01cc4c54
Anthony <anthonycolas.france@gmail.com>
2023-02-19 20:19:42 +0100

    Login  button

--- components/Login.tsx
+++ components/Login.tsx
@@ -5,14 +5,19 @@ import Image from "next/image";
 
 function Login() {
   return (
-    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center">
+    <div className="bg-[#11A37F] h-screen flex flex-col items-center justify-center text-center">
       <Image
         src="https://links.papareact.com/2i6"
         width={300}
         height={300}
         alt="MoneypennyGPTLogo"
       />
-      <button>Sign in to MoneypennyGPT</button>
+      <button
+        onClick={() => signIn("google")}
+        className="text-white font-bold text-3xl animate-pulse"
+      >
+        Sign in to MoneypennyGPT
+      </button>
     </div>
   );
 }

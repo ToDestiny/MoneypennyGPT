31395cdc117239e297a98ea32b5d3b63be511443
Anthony <anthonycolas.france@gmail.com>
2023-02-21 20:11:19 +0100

    Feature: Remove Chat OK

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -37,7 +37,7 @@ function SideBar() {
         // eslint-disable-next-line @next/next/no-img-element
         <img
           onClick={() => signOut()}
-          src={session.user?.image!}
+          src={session?.user?.image!}
           alt="Profile picture"
           className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
         />

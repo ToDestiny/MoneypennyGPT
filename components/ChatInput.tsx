fb3ec4b0ac7ace33f605cdec72de4829fcf4b925
Anthony <anthonycolas.france@gmail.com>
2023-02-24 20:17:44 +0100

    Input message: ongoing

--- components/ChatInput.tsx
+++ components/ChatInput.tsx
@@ -2,7 +2,7 @@
 
 import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 type Props = {
   chatId: string;
@@ -12,9 +12,17 @@ function ChatInput({ chatId }: Props) {
   const [prompt, setPrompt] = useState("");
   const { data: session } = useSession();
 
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+  };
+
   return (
     <div className="bg-gray-700/50 text-gray-400 rounded-lg text-sm">
-      <form className="p-5 space-x-5 flex-1">
+      <form onSubmit={sendMessage} className="p-5 space-x-5 flex-1">
         <input
           className="bg-transparent focus:outline-none flex-1 
 					disabled:cursor-not-allowed  disabled:text-gray-300"

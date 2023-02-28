0ccdbf073919970a2bfd8af55612e8d73042a9ab
Anthony <anthonycolas.france@gmail.com>
2023-02-28 22:02:51 +0100

    ModelSelection : ongoing

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,4 +1,11 @@
+"use client";
+
+import useSWR from "swr";
+
+const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
+
 function ModelSelection() {
+  const { data: models, isLoading } = useSWR("models", fetchModels);
   return <div>ModelSelection</div>;
 }
 

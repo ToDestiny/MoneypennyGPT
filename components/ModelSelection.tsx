74fa907dd2f5b48fb2215981ed14964cd170f589
Anthony <anthonycolas.france@gmail.com>
2023-03-01 20:36:04 +0100

    useSWR

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -1,12 +1,22 @@
 "use client";
 
 import useSWR from "swr";
+import Select from "react-select";
 
 const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
 
 function ModelSelection() {
   const { data: models, isLoading } = useSWR("models", fetchModels);
-  return <div>ModelSelection</div>;
+  return (
+    <div>
+      <Select 
+        className="mt-2"
+        // defaultValue={model}
+        isSearchable
+        isLoading={isLoading}
+      /> 
+    </div>
+  );
 }
 
 export default ModelSelection;

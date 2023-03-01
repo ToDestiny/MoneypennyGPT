e2ed1fe2738a30f25e80ef34f536bdf216675740
Anthony <anthonycolas.france@gmail.com>
2023-03-01 20:55:48 +0100

    ModelSelection OK

--- components/ModelSelection.tsx
+++ components/ModelSelection.tsx
@@ -7,14 +7,25 @@ const fetchModels = () => fetch("/api/getEngines").then((res) => res.json());
 
 function ModelSelection() {
   const { data: models, isLoading } = useSWR("models", fetchModels);
+  const { data: model, mutate: setModel } = useSWR("model", {
+    fallbackData: "text-davinci-003",
+  });
+
   return (
     <div>
-      <Select 
+      <Select
         className="mt-2"
-        // defaultValue={model}
+        options={models?.modelOptions}
+        defaultValue={model}
+        placeholder={model}
         isSearchable
         isLoading={isLoading}
-      /> 
+        menuPosition="fixed"
+        classNames={{
+          control: (state) => "bg-[#434654] border-[#434654]",
+        }}
+        onChange={(e) => setModel(e.value)}
+      />
     </div>
   );
 }
